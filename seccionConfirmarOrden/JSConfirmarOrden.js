f2e0706a46b7088224dd0891a61a597641912376
Mpaez14 <matiaspaezws@gmail.com>
2024-10-20 19:25:05 -0300

    front ConfirmarPedido y ConfirmarOrden
    no links

---
+++ seccionConfirmarOrden/JSConfirmarOrden.js
@@ -0,0 +1,6 @@
+function escucharPedido() {
+    const mensaje = "Hamburguesa simple, cantidad: 2. Papas grandes, cantidad: 1.";
+    const speech = new SpeechSynthesisUtterance(mensaje);
+    speech.lang = 'es-ES';
+    window.speechSynthesis.speak(speech);
+}
