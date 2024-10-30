4246aeac1ee2ec6c593187f7a8181d2d1deece69
sasham14 <sashamedina144@gmail.com>
2024-10-30 19:00:35 -0300

    Pedidos: opciones de pedidos guardadas con json. Confirmar Orden: vista modificada y opciones del pedido cargadas en la vista

--- Pedido/pedido.js
+++ Pedido/pedido.js
@@ -1,81 +1,107 @@
-window.onload = inicio ();
+window.onload = inicio();
 
-function inicio () {
-let cantidad = 1;
-const cantidadElemento = document.getElementById('cantidad-texto');
-const btnIncrementar = document.getElementById('mas');
-const btnDecrementar = document.getElementById('menos');
-const selectTamaño = document.getElementById('select-tamaño');
-const precioInput = document.getElementById("precio");
-
-const actualizarPrecio = () => {
-
-    switch (selectTamaño.value) {
-        case 'p':
-            precio = 100;
-            break;
-        case 'm':
-            precio = 150;
-            break;
-        case 'g':
-            precio = 200;
-            break;
+function inicio() {
+    let cantidad = 1;
+    const cantidadElemento = document.getElementById('cantidad-texto');
+    const btnIncrementar = document.getElementById('mas');
+    const btnDecrementar = document.getElementById('menos');
+    const selectTamaño = document.getElementById('select-tamaño');
+    const precioInput = document.getElementById("precio");
+    const nombre = document.getElementById("nombre");
+    const img = document.getElementById("imagen");
+    const boton = document.getElementById("boton-seguir");
+    
+    function simularCarga() {
+        let hamburguesa = '{ "nombre" : "Hamburguesa Simple" , "img_src" : "../categorias/imgs-opciones/hamburguesa-simple.png"}';
+        const obj = JSON.parse(hamburguesa);
+        nombre.textContent = obj.nombre;
+        img.src = obj.img_src;
     }
-    let precioMultiplicado = cantidad * precio;
-    precioInput.value = precioMultiplicado;
-};
 
-btnIncrementar.addEventListener('click', () => {
-    if (cantidad < 10) {
-        cantidad++;
-        cantidadElemento.textContent = cantidad;
-        actualizarPrecio(); 
+    simularCarga();
+
+
+    function guardarPedido() {
+        let pedido = {
+            "cantidad": cantidadElemento.textContent,
+            "tamaño": selectTamaño.options[selectTamaño.selectedIndex].text,
+            "nombre": nombre.textContent,
+            "imagen": img.src,
+            "precio": precioInput.value
+        };
+
+        localStorage.setItem("pedido", JSON.stringify(pedido));
+        console.log("Pedido guardado:", pedido);
     }
-});
 
-btnDecrementar.addEventListener('click', () => {
-    if (cantidad > 1) {
-        cantidad--;
-        cantidadElemento.textContent = cantidad;
-        actualizarPrecio(); 
-    }
-});
+    boton.addEventListener("click", guardarPedido);
 
-selectTamaño.addEventListener('change', () => {
+    const actualizarPrecio = () => {
+        let precio; 
+        switch (selectTamaño.value) {
+            case 'p':
+                precio = 100;
+                break;
+            case 'm':
+                precio = 150;
+                break;
+            case 'g':
+                precio = 200;
+                break;
+            default:
+                precio = 0; 
+        }
+        let precioMultiplicado = cantidad * precio;
+        precioInput.value = precioMultiplicado;
+    };
+
+    btnIncrementar.addEventListener('click', () => {
+        if (cantidad < 10) {
+            cantidad++;
+            cantidadElemento.textContent = cantidad;
+            actualizarPrecio();
+        }
+    });
+
+    btnDecrementar.addEventListener('click', () => {
+        if (cantidad > 1) {
+            cantidad--;
+            cantidadElemento.textContent = cantidad;
+            actualizarPrecio();
+        }
+    });
+
+    selectTamaño.addEventListener('change', () => {
+        actualizarPrecio();
+    });
+
     actualizarPrecio();
-});
 
-
-actualizarPrecio();
-
-
-
-function leerContenido() {
-    const synth = window.speechSynthesis;
-    const texto = obtenerTextoDeLaPagina();
-    if (synth.speaking) {
-        synth.cancel();
+    function leerContenido() {
+        const synth = window.speechSynthesis;
+        const texto = obtenerTextoDeLaPagina();
+        if (synth.speaking) {
+            synth.cancel();
+        }
+        if (texto !== '') {
+            const utterance = new SpeechSynthesisUtterance(texto);
+            synth.speak(utterance);
+        }
     }
-    if (texto !== '') {
-        const utterance = new SpeechSynthesisUtterance(texto);
-        synth.speak(utterance);
+
+    function obtenerTextoDeLaPagina() {
+        let texto = '';
+
+        const titulo = document.querySelector('h3').textContent;
+        const cantidad = document.getElementById('cantidad-texto').textContent; 
+        const tamañoSeleccionado = document.getElementById('select-tamaño').value;
+        const tamañoTexto = document.querySelector(`#select-tamaño option[value="${tamañoSeleccionado}"]`).textContent;
+        const precio = document.getElementById('precio').value; 
+
+        texto += `${titulo}. Cantidad: ${cantidad}. Tamaño: ${tamañoTexto}. Precio: ${precio}.`; 
+        return texto;
     }
-}
- 
-function obtenerTextoDeLaPagina() {
-    let texto = '';
- 
-    const titulo = document.querySelector('h3').textContent;
-    const cantidad = document.getElementById('cantidad-texto').textContent; 
-    const tamañoSeleccionado = document.getElementById('select-tamaño').value;
-    const tamañoTexto = document.querySelector(`#select-tamaño option[value="${tamañoSeleccionado}"]`).textContent;
-    const precio = document.getElementById('precio').value; 
 
-    texto += `${titulo}. Cantidad: ${cantidad}. Tamaño: ${tamañoTexto}. Precio: ${precio}.`; 
-    return texto;
-}
- 
-const btnParlante = document.getElementById('btn-parlante');
-btnParlante.addEventListener('click', leerContenido);
-
+    const btnParlante = document.getElementById('btn-parlante');
+    btnParlante.addEventListener('click', leerContenido);
 }
---
+++ ver-orden.js
@@ -0,0 +1,27 @@
+window.onload = inicio;
+
+function inicio() {
+    const cantidad_o1 = document.getElementById('cantidad-orden1');
+    const tamaño_o1 = document.getElementById('tamaño-orden1');
+    const nombre_o1 = document.getElementById("nombre-orden1");
+    const img_o1 = document.getElementById("imagen-orden1");
+    const precio_o1 = document.getElementById("precio-orden1");
+
+    getPedido(cantidad_o1, tamaño_o1, nombre_o1, img_o1, precio_o1);
+}
+
+function getPedido(cantidad_o1, tamaño_o1, nombre_o1, img_o1, precio_o1) {
+    const pedido = JSON.parse(localStorage.getItem('pedido'));
+
+    console.log(pedido);
+
+    if (pedido) {
+        cantidad_o1.textContent = pedido.cantidad;
+        tamaño_o1.textContent = pedido.tamaño;
+        nombre_o1.textContent = pedido.nombre;
+        img_o1.src = pedido.imagen;
+        precio_o1.textContent = pedido.precio;
+    } else {
+        console.log("No hay pedidos!!!");
+    }
+}
